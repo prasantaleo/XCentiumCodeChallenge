da97947a01defaee7213d0857c02175dc5a7ed62
prasanta kumar mohanty <prasanta.mohanty@hotmail.com>
2025-03-30 23:27:15 +0530

    changes

--- URLAnalyzer/Models/UrlAnalysisResultModel.cs
+++ URLAnalyzer/Models/UrlAnalysisResultModel.cs
@@ -3,7 +3,7 @@
     public class UrlAnalysisResultModel
     {
         public List<string> ImageUrls { get; set; } = new List<string>();
-        public int WordCount { get; set; }
+        public int WordCount { get; set; } 
         public Dictionary<string, int> TopWords { get; set; } = new Dictionary<string, int>();
 
     }
